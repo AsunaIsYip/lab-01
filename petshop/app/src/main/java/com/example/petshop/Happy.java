35b0ca5170ae081d06f5320794228f5993331c32
AsunaIsYip <156797289+AsunaIsYip@users.noreply.github.com>
2026-01-09 12:24:44 -0700

    Add files via upload

---
+++ petshop/app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,18 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood{
+
+    public Happy() {
+        super();
+    }
+
+    public Happy(Date date) {
+        super(date);
+    }
+
+    public String showMood() {
+        return "Happy";
+    }
+}
